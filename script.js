b3748a47caea7fd4642a0c133de8ffcded559a58
thejustinlombardi <thejustinlombardi@gmail.com>
2021-12-07 00:08:53 -0500

    Add CSS style to buttons

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ howToBtn.addEventListener("click", openHowToModal);
 openBtn.addEventListener("click", openModal);
 close.addEventListener("click", closeModal);
 closeHowToBtn.addEventListener("click", closeHowToModal);
+
+const simonArr = [];
+const playerArr = [];
+
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setSimonColor() {
+	if (getRandomNumber(1, 4) === 1) {
+		simonArr.push("R");
+	} else if (getRandomNumber(1, 4) === 2) {
+		simonArr.push("G");
+	} else if (getRandomNumber(1, 4) === 3) {
+		simonArr.push("Y");
+	} else if (getRandomNumber(1, 4) === 4) {
+		simonArr.push("B");
+	}
+}
+setSimonColor();
+console.log(simonArr);

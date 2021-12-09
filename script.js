aa00c8bf38e089b10de67e83325a97dea9335dd9
thejustinlombardi <thejustinlombardi@gmail.com>
2021-12-09 12:12:36 -0500

    Add functionality to Simon and Player

--- script.js
+++ script.js
@@ -42,12 +42,16 @@ const PLAYERS = {
 };
 
 /*----- app's state (variables) -----*/
-let simonArr;
-let playerArr;
-let currentPlayer;
-let round;
+let simonArr = [];
+let playerArr = [];
+let round = 0;
 let winStatus;
 
+// for (let i = 0; i < 50; i++) {
+// 	setSimonColor(getRandomNumber(1, 4));
+// }
+// console.log(simonArr);
+// getSimonMove(event, simonArr, round);
 /*----- cached element references -----*/
 const gameBoard = document.querySelector(".game-board");
 const redDiv = document.querySelector(".red-button");
@@ -59,11 +63,6 @@ const resetBtn = document.querySelector(".reset-btn");
 let playerH3 = document.querySelector(".player-turn");
 let roundNum = document.querySelector(".round-num");
 
-/*----- event listeners -----*/
-gameBoard.addEventListener("click", handleClick);
-resetBtn.addEventListener("click", init);
-playBtn.addEventListener("click", init);
-
 /*----- functions -----*/
 function init(event) {
 	simonArr = [];
@@ -73,90 +72,66 @@ function init(event) {
 	roundNum.innerText = `Round: ${round}`;
 	winStatus = true;
 	playBtn.style.display = "block";
-	render();
+	playerH3.innerText = "SIMON SAYS...";
 }
 
-function render() {
-	simonArr = [];
-	playerArr = [];
-	currentPlayer = PLAYERS.playerOne;
-	round = 0;
+function gamePlay(event, simonArr) {
 	roundNum.innerText = `Round: ${round}`;
 	winStatus = true;
 	for (let i = 0; i < 50; i++) {
 		setSimonColor(getRandomNumber(1, 4));
 	}
-	playerH3.innerText = "Simon Says...";
-	gamePlay(event, playerArr, simonArr);
+	playerH3.innerText = "SIMON SAYS...";
+
+	getSimonMove(simonArr, round);
 }
 
-function handleClick(event) {
-	getPlayerMove(event, playerArr, round, simonArr);
-}
+// function checkWinner() {
+// 	while (
+// 		playerArr.length !== simonArr.length &&
+// 		playerArr[round] === simonArr[round]
+// 	) {}
+// }
 
-function gamePlay(event, playerArr, simonArr) {
-	round = 0;
-	if ((currentPlayer = PLAYERS.playerOne)) {
-		getSimonMove(simonArr, round);
+function getPlayerMove(event, playerArr, round) {
+	if (event.target.id === "red-button") {
+		redDiv.classList.add("red-button-glow");
+		setTimeout(() => {
+			redDiv.classList.remove("red-button-glow");
+		}, 1000);
+		playerArr.push("R");
+	} else if (event.target.id === "green-button") {
+		greenDiv.classList.add("green-button-glow");
+		setTimeout(() => {
+			greenDiv.classList.remove("green-button-glow");
+		}, 1000);
+		playerArr.push("G");
+	} else if (event.target.id === "yellow-button") {
+		yellowDiv.classList.add("yellow-button-glow");
+		setTimeout(() => {
+			yellowDiv.classList.remove("yellow-button-glow");
+		}, 1000);
+		playerArr.push("Y");
+	} else if (event.target.id === "blue-button") {
+		blueDiv.classList.add("blue-button-glow");
+		setTimeout(() => {
+			blueDiv.classList.remove("blue-button-glow");
+		}, 1000);
+		playerArr.push("B");
 	}
-	switchCurrentPlayer();
-	if ((currentPlayer = PLAYERS.playerTwo)) {
-	}
-	round++;
-}
 
-function checkWinner() {
-	while (
-		playerArr.length !== simonArr.length &&
-		playerArr[round] === simonArr[round]
-	) {}
-
-	function switchCurrentPlayer() {
-		if (currentPlayer === PLAYERS.playerOne) {
-			currentPlayer = PLAYERS.playerTwo;
-		} else {
-			currentPlayer = PLAYERS.playerOne;
-		}
-	}
-
-	function getPlayerMove(event, playerArr, round, simonArr) {
-		playerH3.innerText = "Player Says...";
-		if (event.target.id === "red-button") {
-			redDiv.classList.add("red-button-glow");
-			setTimeout(() => {
-				redDiv.classList.remove("red-button-glow");
-			}, 1000);
-			playerArr.push("R");
-		} else if (event.target.id === "green-button") {
-			greenDiv.classList.add("green-button-glow");
-			setTimeout(() => {
-				greenDiv.classList.remove("green-button-glow");
-			}, 1000);
-			playerArr.push("G");
-		} else if (event.target.id === "yellow-button") {
-			yellowDiv.classList.add("yellow-button-glow");
-			setTimeout(() => {
-				yellowDiv.classList.remove("yellow-button-glow");
-			}, 1000);
-			playerArr.push("Y");
-		} else if (event.target.id === "blue-button") {
-			blueDiv.classList.add("blue-button-glow");
-			setTimeout(() => {
-				blueDiv.classList.remove("blue-button-glow");
-			}, 1000);
-			playerArr.push("B");
-		}
-	}
+	// checkWinner();
 }
 
 function getSimonMove(simonArr, round) {
-	playBtn.style.display = "none";
-	playerH3.innerText = "Simon Says...";
 	for (let i = 0; i <= round; i++) {
 		setTimeout(() => {
 			playSimonColors(simonArr, i);
 		}, 1000 * i);
 	}
+	setTimeout(() => {
+		playerH3.innerText = "PLAYER SAYS...";
+	}, 1500 * 1);
 }
 
 function playSimonColors(simonArr, i) {
@@ -206,3 +181,15 @@ function setSimonColor(num) {
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+/*----- event listeners -----*/
+gameBoard.addEventListener("click", function (event) {
+	getPlayerMove(event, playerArr);
+});
+resetBtn.addEventListener("click", init);
+playBtn.addEventListener("click", function (event) {
+	playBtn.style.display = "none";
+	setTimeout(() => {
+		gamePlay(event, simonArr);
+	}, 500);
+});
